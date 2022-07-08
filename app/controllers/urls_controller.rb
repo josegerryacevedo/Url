6048d38eec1544f31fd9ed665c9a34273dfd6575
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-08 18:05:03 +0800

    Create Logout Session, Create redirect & 404 error

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -13,6 +13,19 @@ class UrlsController < ApplicationController
     @url = Url.new
   end
 
+
+  def redirect
+    @short_ext = params[:short_url]
+    @url=Url.find_by(short_url: @short_ext)
+    if @url.nil?
+      render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+    else
+      @url.increment!(:count, 1)
+      redirect_to @url.long_url
+    end
+  end
+
+
   def create
     @url = Url.new(url_params)
     @url.user = current_user
@@ -23,11 +36,13 @@ class UrlsController < ApplicationController
     end
   end
 
+
   def destroy
     if @url.destroy
       redirect_to urls_path
     end
   end
+
 
 
   def set_own_url
@@ -40,9 +55,11 @@ class UrlsController < ApplicationController
 
   private
 
+
   def url_params
     params.require(:url).permit(:description, :long_url)
   end
+
 
   def set_url
     @url = Url.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/urls' => 'urls#index'
+  get '/:short_url' => 'urls#redirect'
   root :to => "urls#index"
-  resources :urls
+  resources :urls, only: [:index, :new, :create, :destroy]
+  get '*path', to: 'urls#not_found'
+
 end
+
+
