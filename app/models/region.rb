30076a2687f652123ab7237a4799183c6a783a8b
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-11 18:50:24 +0800

    Region Model

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,7 @@ class Region < ApplicationRecord
   validates_presence_of :code
   validates_presence_of :name
   validates_presence_of :region_name
+
+  has_many :provinces
+  has_many :district
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root :to => "urls#index"
   get '/urls' => 'urls#index'
   get '/:short_url' => 'urls#redirect'
-  root :to => "urls#index"
+
   resources :urls, only: [:index, :new, :create, :destroy]
   get '*path', to: 'urls#not_found'
 
