ba61e733dc921ac2b6de0fda86b42c02d1d92a24
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-11 18:51:39 +0800

    Edit Format "readable"

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,7 +3,6 @@ class UrlsController < ApplicationController
   before_action :set_url, only: :show
   before_action :set_own_url, only: :destroy
 
-
   def index
     @urls = Url.all.includes(:user)
     @domain = request.base_url
@@ -13,18 +12,16 @@ class UrlsController < ApplicationController
     @url = Url.new
   end
 
-
   def redirect
     @short_ext = params[:short_url]
-    @url=Url.find_by(short_url: @short_ext)
+    @url = Url.find_by(short_url: @short_ext)
     if @url.nil?
-      render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+      not_found
     else
       @url.increment!(:count, 1)
       redirect_to @url.long_url
     end
   end
-
 
   def create
     @url = Url.new(url_params)
@@ -36,14 +33,11 @@ class UrlsController < ApplicationController
     end
   end
 
-
   def destroy
     if @url.destroy
       redirect_to urls_path
     end
   end
-
-
 
   def set_own_url
     @url = current_user.urls.find_by_id(params[:id])
@@ -55,15 +49,12 @@ class UrlsController < ApplicationController
 
   private
 
-
   def url_params
     params.require(:url).permit(:description, :long_url)
   end
 
-
   def set_url
     @url = Url.find(params[:id])
   end
-
 
 end
