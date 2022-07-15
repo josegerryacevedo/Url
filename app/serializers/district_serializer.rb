3c81d26bb08aa104807d53751977da3b73cd9238
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-15 13:47:53 +0800

    Has many connect to city & municipality

--- app/serializers/district_serializer.rb
+++ app/serializers/district_serializer.rb
@@ -1,6 +1,6 @@
 class DistrictSerializer < ActiveModel::Serializer
   attributes :code, :name, :region_id
   belongs_to :region, serializer: RegionSerializer
-
+  has_many :city_municipalities
 
 end
--- app/serializers/province_serializer.rb
+++ app/serializers/province_serializer.rb
@@ -1,6 +1,7 @@
 class ProvinceSerializer < ActiveModel::Serializer
   attributes :code, :name, :region_id
   belongs_to :region, serializer: RegionSerializer
+  has_many :city_municipalities
 
 
 end
